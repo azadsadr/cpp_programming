65bdd487d93f66ccb7be6221d0104a39eab9c3ae
azad <azad.sadr.h@gmail.com>
2020-12-01 12:02:04 +0100

    exercise added

---
+++ exercises/01_compiler_messages.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+    return 0;
+}
+
+
+/*
+ * do not put the main function                                 --->
+ * typos in function name (e.g., c0ut)                          --->
+ * typos in variable name                                       --->
+ * forget to put ;                                              --->
+ * forget to #include <iostream>                                --->
+ * declare a variable with illegal name like int double {0};    --->
+ */
