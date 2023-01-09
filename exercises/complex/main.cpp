8efafd63353c18aa5b0cb2dedf0f14e3e371d061
azadsadr <azad.sadr.h@gmail.com>
2023-01-09 16:11:26 +0100

    applied template to complex number problem

--- exercises/complex/main.cpp
+++ exercises/complex/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
+template <typename T>
 class CComplex {
 public:
-    double real;
-    double imag;
-    CComplex(const double& a, const double& b) {
+    T real;
+    T imag;
+    CComplex(const T& a, const T& b) {
         real = a;
         imag = b;
     }
@@ -12,11 +13,12 @@ public:
     CComplex operator - (const CComplex& obj);
     CComplex operator * (const CComplex& obj);
     CComplex operator / (const CComplex& obj);
-    friend std::ostream& operator << (std::ostream& os, const CComplex& obj);
+    template <typename V>
+    friend std::ostream& operator << (std::ostream& os, const CComplex<V>& obj);
 };
 
-
-std::ostream& operator << (std::ostream& os, const CComplex& obj) {
+template <typename T>
+std::ostream& operator << (std::ostream& os, const CComplex<T>& obj) {
     if (obj.imag > 0) {
         os << obj.real << " + " << obj.imag << "i\n";
     } else if (obj.imag < 0) {
@@ -27,31 +29,33 @@ std::ostream& operator << (std::ostream& os, const CComplex& obj) {
     return os;
 }
 
-
-CComplex CComplex::operator + (const CComplex& obj) {
+template <typename T>
+CComplex<T> CComplex<T>::operator + (const CComplex<T>& obj) {
     CComplex result(0,0);
     result.real = real + obj.real;
     result.imag = imag + obj.imag;
     return result;
 }
 
-
-CComplex CComplex::operator - (const CComplex& obj) {
-    CComplex result(0,0);
+template <typename T>
+CComplex<T> CComplex<T>::operator - (const CComplex<T>& obj) {
+    CComplex<T> result(0,0);
     result.real = real - obj.real;
     result.imag = imag - obj.imag;
     return result;
 }
 
-CComplex CComplex::operator * (const CComplex& obj) {
-    CComplex result(0,0);
+template <typename T>
+CComplex<T> CComplex<T>::operator * (const CComplex<T>& obj) {
+    CComplex<T> result(0,0);
     result.real = real * obj.real - imag * obj.imag;
     result.imag = real * obj.imag + imag * obj.real;
     return result;
 }
 
-CComplex CComplex::operator / (const CComplex& obj) {
-    CComplex result(0,0);
+template <typename T>
+CComplex<T> CComplex<T>::operator / (const CComplex<T>& obj) {
+    CComplex<T> result(0,0);
     result.real = ((real*obj.real)+(imag*obj.imag))/(obj.real*obj.real + obj.imag*obj.imag);
     result.imag = (imag*obj.real - real*obj.imag)/(obj.real*obj.real + obj.imag*obj.imag);
     return result;
@@ -59,8 +63,8 @@ CComplex CComplex::operator / (const CComplex& obj) {
 
 
 int main() {
-    CComplex c1(2,4);
-	CComplex c2(1,6);
+    CComplex<double> c1(2,4);
+	CComplex<double> c2(1,6);
 	std::cout << "c1: " << c1;
 	std::cout << "c2: " << c2;
 	std::cout << "c1+c2: " << c1+c2;
