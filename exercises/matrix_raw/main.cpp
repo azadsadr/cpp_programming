fb4715f5340030f4f5a067e3a71c8920021aad84
azadsadr <azad.sadr.h@gmail.com>
2023-01-11 18:43:31 +0100

    add square matrix class using raw pointer

---
+++ exercises/matrix_raw/main.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+
+template <typename T>
+class CMatrix {
+public:
+    int size;
+    T* data;
+
+    CMatrix();                                      // default constructor
+    CMatrix(const int& N);                          // parameterized constructor
+    CMatrix(const CMatrix<T>& obj);                 // copy constructor
+    CMatrix<T>& operator = (const CMatrix<T>& obj); // copy assignment operator
+    CMatrix(CMatrix<T>&& obj);                      // move constructor
+    CMatrix<T>& operator = (CMatrix<T>&& obj);      // move assignment operator
+    ~CMatrix();                                     // destructor
+
+    void importMatrix(const std::string& file);
+    void exportMatrix(const std::string& file);
+    void printMatrix();
+    CMatrix<T> operator * (const CMatrix<T>& B);
+};
+
+
+// default constructor
+template<typename T>
+CMatrix<T>::CMatrix() {
+    size=0;
+    data = nullptr;
+    std::cout << "default constructor called!\n";
+}
+
+// parameterized constructor
+template <typename T>
+CMatrix<T>::CMatrix(const int& N) {
+    size = N;
+    if (N>0) {
+        data = new T[size*size];
+    } else {
+        data = nullptr;
+    }
+    std::cout << "parameterized constructor called!\n";
+}
+
+// copy constructor
+template <typename T>
+CMatrix<T>::CMatrix(const CMatrix<T>& obj) {
+    size = obj.size;
+    //data = nullptr;
+    if (obj.data == nullptr) {
+        data = nullptr;
+    } else {
+        data = new T[size*size];
+        for (int i=0; i<size*size; i++) {
+            data[i] = obj.data[i];
+        }
+    }
+    std::cout << "copy constructor called!\n";
+}
+
+// copy assignment operator
+template <typename T>
+CMatrix<T>& CMatrix<T>::operator = (const CMatrix<T>& obj) {
+    if (this != &obj) {
+        size = obj.size;
+
+        if (data != nullptr) {
+            delete[] data;
+            data = nullptr;
+        }
+
+        if (obj.data != nullptr) {
+            data = new T[size*size];
+            for (int i=0; i<size*size; i++) {
+                data[i] = obj.data[i];
+            }
+        } else {
+            data = nullptr;
+        }
+    }
+    std::cout << "copy assignment operator called!\n";
+    return *this;
+}
+
+// move constructor
+template <typename T>
+CMatrix<T>::CMatrix(CMatrix<T>&& obj) {
+    if (this != &obj) {
+        size = obj.size;
+        obj.size = 0;
+        data = obj.data;
+        obj.data = nullptr;
+    }
+    std::cout << "move constructor called!\n";
+}
+
+
+// move assignment operator
+template <typename T>
+CMatrix<T>& CMatrix<T>::operator = (CMatrix<T>&& obj) {
+    if (this != &obj) {
+        size = obj.size;
+        obj.size = 0;
+        if (data != nullptr) {
+            delete[] data;
+            data = nullptr;
+        }
+        data = obj.data;
+        obj.data = nullptr;
+    }
+    return *this;
+    std::cout << "move assignment operator called!\n";
+}
+
+// destructor
+template <typename T>
+CMatrix<T>::~CMatrix() {
+    if (data != nullptr) {
+        delete[] data;
+        data = nullptr;
+    }
+    std::cout << "destructor called!\n";
+}
+
+
+template <typename T>
+void CMatrix<T>::importMatrix(const std::string& file) {
+
+    std::ifstream filevar(file);
+    if (filevar) {
+        std::cout << "successfully read the file " << file << "\n";
+        filevar >> size;
+        data = new T[size*size];
+        for (int i=0; i<size; i++) {
+            for (int j=0; j<size; j++) {
+                filevar >> data[i*size+j];
+            }
+        }
+        filevar.close();
+    } else {
+        std::cout << "couldn't open the file " << file << "\n";
+        exit(1);
+    }
+}
+
+template <typename T>
+void CMatrix<T>::exportMatrix(const std::string& file) {
+    std::ofstream filevar(file);
+    if (filevar) {
+        filevar << size << "\n";
+        if (data != nullptr) {
+            for (int i=0; i<size; i++) {
+                for (int j=0; j<size; j++) {
+                    filevar << data[i*size+j] << " ";
+                }
+                filevar << "\n";
+            }
+            filevar.close();
+            std::cout << "export matrix to file " << file << "\n";
+        } else {
+            std::cout << "matrix is null, nothing to export!\n";
+        }
+    } else {
+        std::cout << "unable to export the matrix to file!\n";
+    }
+}
+
+
+template <typename T>
+void CMatrix<T>::printMatrix() {
+    for (int i=0; i<size; i++) {
+        for (int j=0; j<size; j++) {
+            std::cout << data[i*size+j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+template <typename T>
+CMatrix<T> CMatrix<T>::operator * (const CMatrix<T>& mat) {
+    if (size != mat.size) {
+        std::cout << "two matrices are not same size, result is nonsense!\n";
+    }
+    CMatrix<T> res(size);
+    for(int i=0; i<size; i++) {
+        for(int j=0; j<size; j++) {
+            for (int k=0; k<size; k++) {
+                res.data[i*size+j] += data[i*size+k] * mat.data[k*size+j];
+            }
+        }
+    }
+    return res;
+}
+
+
+
+int main() {
+
+    CMatrix<double> A, B;
+    std::cout << "matrix A and B created\n";
+    std::cout << "==============\n";
+    A.importMatrix("A.txt");
+    B.importMatrix("B.txt");
+    std::cout << "==============\n";
+    std::cout << "Matrix A:\n";
+    A.printMatrix();
+    std::cout << "Matrix B\n";
+    B.printMatrix();
+    std::cout << "==============\n";
+    auto C = A*B;
+    std::cout << "Matrix C = A*B:\n";
+    C.printMatrix();
+    C.exportMatrix("C.txt");
+    std::cout << "==============\n";
+    std::cout << "C = A:\n";
+    C = A;
+    C.printMatrix();
+    std::cout << "==============\n";
+    std::cout << "CMatrix<double> D(A):\n";
+    CMatrix<double> D(A);
+    D.printMatrix();
+    std::cout << "==============\n";
+    std::cout << "C = std::move(A)\n";
+    C = std::move(A);
+    C.printMatrix();
+    std::cout << "==============\n";
+    std::cout << "Matrix A:\n";
+    A.printMatrix();
+    std::cout << "==============\n";
+
+    return 0;
+}
